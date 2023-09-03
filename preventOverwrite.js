61e989f7a5621e6815de7a4d83e7636722aadee1
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2023-09-03 12:46:37 +0300

    Create preventOverwrite.js

---
+++ preventOverwrite.js
@@ -0,0 +1,29 @@
+// Select the existing button and textarea
+const existingBtn = document.querySelector("button#no");
+const textarea = document.querySelector("textarea");
+
+// Create a new button
+const newBtn = document.createElement("button");
+newBtn.textContent = "New Button"; // Set the button text as an example
+
+// Flag to prevent overwriting the text
+let preventOverwrite = false;
+
+// Add a click event listener to the new button
+newBtn.addEventListener("click", () => {
+    if (!preventOverwrite) {
+        textarea.focus();
+        textarea.value = "special_item"; // Set the comment directly
+        textarea.dispatchEvent(new Event("input", { bubbles: true })); // Trigger the input event
+        preventOverwrite = true;
+    }
+    existingBtn.click();
+});
+
+// Add an input event listener to the textarea to reset the flag
+textarea.addEventListener("input", () => {
+    preventOverwrite = false;
+});
+
+// Append the new button to the same parent as the existing button
+existingBtn.parentElement.appendChild(newBtn);

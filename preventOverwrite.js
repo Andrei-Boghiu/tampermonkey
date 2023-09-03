c38d4862cfcafc4b7f5d257d59a7047294ac51c1
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2023-09-03 12:50:57 +0300

    Update preventOverwrite.js

--- preventOverwrite.js
+++ preventOverwrite.js
@@ -27,3 +27,27 @@ textarea.addEventListener("input", () => {
 
 // Append the new button to the same parent as the existing button
 existingBtn.parentElement.appendChild(newBtn);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Select the existing button and textarea
+const existingBtn = document.querySelector("button#no");
+const textarea = document.querySelector("textarea");
+
+// Create a new button
+const newBtn = document.createElement("button");
+newBtn.textContent = "New Button"; // Set the button text as an example
+
+// Add a click event listener to the new button
+newBtn.addEventListener("click", () => {
+    textarea.focus();
+    textarea.value = "special_item"; // Set the comment directly
+    textarea.dispatchEvent(new Event("input", { bubbles: true })); // Trigger the input event
+    textarea.dispatchEvent(new Event("change", { bubbles: true })); // Trigger a change event
+    existingBtn.click();
+});
+
+// Append the new button to the same parent as the existing button
+existingBtn.parentElement.appendChild(newBtn);
